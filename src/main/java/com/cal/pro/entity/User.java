ec58fb480d926f517682f9ab0ecc4e240bb2d86b
Murat Çelik <muratcelik11@gmail.com>
2021-06-27 15:57:45 +0300

    Initial commit

---
+++ src/main/java/com/cal/pro/entity/User.java
@@ -0,0 +1,58 @@
+package com.cal.pro.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "t_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String firstname;
+    private String lastname;
+    private String mobile;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
